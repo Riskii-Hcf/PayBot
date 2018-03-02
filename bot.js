8c8e78903cdef35a65af34b9720f7f52a0bf9889
Riskii <34557522+Riskii-Hcf@users.noreply.github.com>
2018-03-02 13:32:28 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -37,8 +37,7 @@ var lt = ["100% In Love",
 "50% In Love With Someone",
 "0% In Love With Someone",
 "20% In Love With Someone",
-"Already Are A Couple With Someone",
-"Your Jealous Of A Couple"];
+"Already Are A Couple With Someone"];
 
 bot.on("ready", function() {
       bot.user.setGame("PayBot Prefix = !" , "https://www.twitch.tv/123silly");
